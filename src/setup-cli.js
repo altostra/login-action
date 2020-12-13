0dcda2f388a6010271f464340a9e67f60713fee4
Yossale <yossi@altostra.com>
2020-12-13 18:43:36 +0200

    update

--- src/setup-cli.js
+++ src/setup-cli.js
@@ -7,7 +7,9 @@ async function setupCli() {
         console.log(`Installing @altosra/cli`)
         await exec.exec('sudo npm i -g @altostra/cli');
 
-        const token = core.getInput('api-token', { required: true });
+        const token = core.getInput('api-tokenx', { required: true });
+        // Register secret to ensure it's masked in logs
+        core.setSecret(token)
 
         console.log(`Setting up credentials:`)
         await exec.exec(`alto api-key set ${token}`);
