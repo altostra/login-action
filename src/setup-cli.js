c6efdaca03f9e4e942b874d9f94d2b7f64a38f70
Yossale <yossi@altostra.com>
2020-12-13 17:39:03 +0200

    bug fix

--- src/setup-cli.js
+++ src/setup-cli.js
@@ -2,17 +2,25 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const exec = require('@actions/exec');
 
-try {
-    console.log(`Installing @altosra/cli`)
-    await exec.exec('npm i -g @altostra/cli');
+async function setupCli() {
+    try {
+        console.log(`Installing @altosra/cli`)
+        await exec.exec('npm i -g @altostra/cli');
 
-    const token = core.getInput('api-token');
+        const token = core.getInput('api-token');
+        if (!!token) {
+            throw new Error("API Token can not be empty")
+        }
 
-    console.log(`Setting up credentials`)
-    await exec.exec(`alto api-key set ${token}`);
+        console.log(`Setting up credentials`)
+        await exec.exec(`alto api-key set ${token}`);
 
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
-} catch (error) {
-    core.setFailed(error.message);
+        const payload = JSON.stringify(github.context.payload, undefined, 2)
+        console.log(`The event payload: ${payload}`);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+
 }
+
+setupCli()
